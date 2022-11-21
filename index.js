ff212a9196a8c0495648c73046816c781899f995
Thamyris <thamyriscabral@hotmail.com>
2022-11-21 15:34:52 -0300

    Iniciando Objetos

---
+++ index.js
@@ -0,0 +1,24 @@
+let pet = {
+  nome: 'Luffy',
+  Idade: 9,
+  beleza: 100,
+
+  funcao: function (...adjetivos) {
+    console.log(`${pet.nome} é ${adjetivos}`);
+  },
+};
+pet.funcao('Lindo', 'Maravilhoso', 'Dengoso');
+
+let amor = {
+  nome: 'Carlos',
+  Idade: 37,
+  amo: 'pra sempre',
+  gosta: {
+    anime: 'One piece',
+    comer: {
+      comida: ['pizza', 'Pipoca'],
+      bebida: 'coca-cola zero',
+    },
+  },
+};
+console.log(amor.gosta.comer.comida[1]);
